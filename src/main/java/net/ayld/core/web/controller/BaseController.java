450b73b8a597050796776d764cf72233fe636258
ayld <iliev.simeon@gmail.com>
2012-08-10 18:17:34 +0300

    - moved all java files from /test to /main (they were not supposed to be there you know ...)

--- src/main/java/net/ayld/core/web/controller/BaseController.java
+++ src/main/java/net/ayld/core/web/controller/BaseController.java
@@ -1,8 +1,13 @@
 package net.ayld.core.web.controller;
 
+import java.io.Serializable;
+
+import net.ayld.core.dto.Dto;
+import net.ayld.core.service.CrudService;
+
 import org.springframework.beans.factory.annotation.Required;
 
-public abstract class BaseController<S> {
+public abstract class BaseController<S extends CrudService<D, I>, D extends Dto<I>, I extends Serializable> {
 
 	private S service;
 	
